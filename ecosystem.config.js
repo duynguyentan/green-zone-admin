256c4d4fee4de16c61472828ed217de1bc3bad5e
duynguyen1910 <duynguyen191001.dev@gmail.com>
2025-04-03 11:52:25 +0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 export const apps = [
   {
-    name: 'green-zone-admin',
-    script: 'npm', // Sử dụng npm để chạy lệnh build
-    args: 'run build && serve -s dist -l 4000', // Build và serve static files
+    name: 'green-zone-admin-build',
+    script: 'npm',
+    args: 'run build', // Chạy build trước
     env: {
       NODE_ENV: 'production',
     },
+    // Sau khi build xong, sẽ tự động chạy serve
+    post_deploy: 'serve -s dist -l 4000',
   },
 ];
